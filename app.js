cfc5c67b839f6c456304c978d9258f9f3a2cd0fc
Kelvin <kelvinpc444@gmail.com>
2025-04-28 23:08:42 -0500

    app

--- app.js
+++ app.js
@@ -10,4 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 
-app.use
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use('/', rutaTienda)
+
+app.listen(PORT, () =>{
+    console.log(`Servidor iniciado en http://localhost:3000`)
+})
